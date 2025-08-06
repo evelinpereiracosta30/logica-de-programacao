debfcbf046697453b8efd202efb9df5189f13456
evelinpereiracosta30 <evelinpereiracosta9@gmail.com>
2025-08-06 16:43:57 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int x; 
+float y;
+float s;
+
+x=10;
+y=2.5;
+s=x-y;
+
+
+ printf("Subtração de x e y eh:%f",s);
+
+return 0;
+}
